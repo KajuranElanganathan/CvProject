2d82af745eefdaf378cef4cb6a2f83d45c9adea5
Kajuran <Kajuran65@gmail.com>
2025-07-08 18:13:04 -0400

    basic input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,64 @@
+import { useState } from 'react';
+import './App.css';
+import GeneralSection from './components/GeneralSection.jsx';
+import EducationSection from './components/EducationSection.jsx';
+import ExperienceSection from './components/ExperienceSection.jsx';
+import Preview from './components/preview.jsx';
+
 function App(){
 
-    return (
-    
-        <div>
-            <h1>"hello world"</h1>
-        </div>
-
+    const [general,setGeneral] = useState(
+        {
+            name:"",
+            email:"",
+            phone:"",
+        }
     );
 
+    const [education,setEducation] = useState(
+        {
+
+            school:"",
+            degree:"",
+            from:"",
+            to:"",
+        }
+    );
+
+    const [experience,setExperience] = useState(
+        {
+
+            company:"",
+            position:"",
+            info:"",
+            from:"",
+            to:"",
+        }
+    );
+
+    return (
+
+        <div class = "app-contianer">
+            <h1>CV-Builder</h1>
+            <div class = "form-section">
+
+                <GeneralSection  general = {general} setGeneral = {setGeneral} />
+                <ExperienceSection education = {education} setEducation = {setEducation} />
+                <EducationSection experience = {experience} setExperience = {setExperience} />
+
+
+            </div>
+
+            <div class = "preview-section">
+
+            <Preview
+            general = {general}
+            education = {education}
+            experience = {experience}
+            />
+            </div>
+        </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import GeneralInfo from "./components/generalinfo.jsx";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GeneralInfo/>
+    <App />
   </StrictMode>,
 )
